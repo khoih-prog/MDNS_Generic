44a87adb785fa0daf5709baf811f680c6886209b
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-09-02 01:29:28 -0400

    Update WiFiRegisteringServicesWithTxtRecord.ino

--- examples/WiFi/WiFiRegisteringServicesWithTxtRecord/WiFiRegisteringServicesWithTxtRecord.ino
+++ examples/WiFi/WiFiRegisteringServicesWithTxtRecord/WiFiRegisteringServicesWithTxtRecord.ino
@@ -124,7 +124,7 @@ void setup()
   Serial.begin(115200);
   while (!Serial);
 
-  Serial.println("Start WiFiRegisteringServicesWithTxtRecord on "); Serial.println(BOARD_NAME);
+  Serial.print("Start WiFiRegisteringServicesWithTxtRecord on "); Serial.println(BOARD_NAME);
   Serial.println(MDNS_GENERIC_VERSION);
 
 #if ( WIFI_NETWORK_WIFININA || WIFI_NETWORK_WIFI101 )
