7468f6e06fe3d92abf71697305ab7395824a533d
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-11-09 12:58:26 -0500

    Update arduino_secrets.h

--- examples/WiFi/WiFiRegisteringServices/arduino_secrets.h
+++ examples/WiFi/WiFiRegisteringServices/arduino_secrets.h
@@ -1,4 +1,2 @@
-#define SECRET_SSID     "HueNet1"
-#define SECRET_PASS     "jenniqqs"
-//#define SECRET_SSID     "your_ssid"
-//#define SECRET_PASS     "your_pass"
+#define SECRET_SSID     "your_ssid"
+#define SECRET_PASS     "your_pass"
